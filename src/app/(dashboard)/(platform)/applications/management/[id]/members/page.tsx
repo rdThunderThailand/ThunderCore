139d9dda28532b8fd9c4b9a9df82233afaa1d42a
Benyapa <benyapa.thon@gmail.com>
2026-07-20 11:43:13 +0700

    feat: integrate ApplicationManagementidMembersClient and hide header/back button elements

--- src/app/(dashboard)/(platform)/applications/management/[id]/members/page.tsx
+++ src/app/(dashboard)/(platform)/applications/management/[id]/members/page.tsx
@@ -1,10 +1,11 @@
+import { ApplicationManagementidMembersClient } from "@/features/platform-members/ApplicationManagementidMembersClient"
 
 export const dynamic = 'force-dynamic'
 
 export default async function ApplicationMembersPage(props: any) {
     return (
         <>
-            <div className="">Applications Members</div>
+            <ApplicationManagementidMembersClient />
 
         </>
 
